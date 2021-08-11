acc210baf6252a00b114b5fc7ebfcc2a48bc2346
LaisAxx <laisasss@hotmail.com>
2021-08-11 15:15:02 -0300

    correçao de arquivo

--- experiencein/usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-
-# experiencein/usuarios/urls.py 
-
-from django.urls import path
-from usuarios.views import RegistrarUsuarioView
-
-urlpatterns = [
- path('registrar/', RegistrarUsuarioView.as_view(), name='registrar')
-]
